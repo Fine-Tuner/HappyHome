a081b7fac1ce1fc084bd079777098254b31895fe
TaejoonPark <joker77z@naver.com>
2025-05-20 21:00:37 +0900

    feat: Category Update API 명세 업데이트

--- frontend-react/src/features/category/api/putUpdate.ts
+++ frontend-react/src/features/category/api/putUpdate.ts
@@ -2,39 +2,23 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { client } from "../../../shared/constants/baseApi";
 import queryKeys from "../../announcement/api/queryKey";
 
-export interface UpdateCategoryParams {
-  announcement_id: string;
-  user_category_id: string;
-  user_id: string;
+export interface CategoryUpdateRequest {
+  id: string;
+  name?: string;
+  comment?: string;
 }
 
-export interface UpdateCategoryBody {
-  name: string;
-  comment: string;
-  is_deleted: boolean;
-  updated_at: string;
-}
-
-export const updateCategory = async (
-  params: UpdateCategoryParams,
-  body: UpdateCategoryBody,
-) => {
-  const response = await client.put("/categories/update", body, { params });
+export const updateCategory = async (data: CategoryUpdateRequest) => {
+  const response = await client.put("/categories/update", data);
   return response.data;
 };
 
 export const useUpdateCategory = () => {
   const queryClient = useQueryClient();
   return useMutation({
-    mutationFn: ({
-      params,
-      body,
-    }: {
-      params: UpdateCategoryParams;
-      body: UpdateCategoryBody;
-    }) => updateCategory(params, body),
+    mutationFn: updateCategory,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: queryKeys.list() });
+      queryClient.invalidateQueries({ queryKey: queryKeys.detail() });
     },
   });
 };
